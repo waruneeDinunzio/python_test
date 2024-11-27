0af035b1d75eeec7d9fda508c2bbde76e5221eb3
Warunee Dinunzio <56564699+waruneeDinunzio@users.noreply.github.com>
2024-11-27 17:22:47 -0500

    Update main.py
    change print()

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 def in_autotests_we_trust(a, b):
     if a == b:
-        print('Test passed')
+        print('PASS')
     else:
-        print('Test failed')
+        print('FAIL')
 
-in_autotests_we_trust('10', '10')
+in_autotests_we_trust(10, '10')
 
 in_autotests_we_trust(0, False)
